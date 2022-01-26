7d195a7e5f52e2019f734432192385dc895df775
Lucas Borges <lucas_borges_br@hotmail.com>
2022-01-25 23:48:30 -0300

    .

--- elibras/server.js
+++ elibras/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { resolve } = require('path')
 const app = express()
-
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(
         resolve(
             __dirname,
@@ -11,7 +11,7 @@ const app = express()
     app.get('*', (req, res) => {
         res.sendFile(resolve(__dirname, 'build', 'index.html'));
       });
-
+}
 app.listen(process.env.PORT || 3000, (err) => {
     if (err) { return console.log(err) }
     console.log("tudo funcionando")
