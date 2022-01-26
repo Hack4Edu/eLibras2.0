593bfe388e41b8afd0476aaa2d146328e66bf384
Lucas Borges <lucas_borges_br@hotmail.com>
2022-01-25 23:43:03 -0300

    fix server

--- elibras/server.js
+++ elibras/server.js
@@ -2,18 +2,17 @@ const express = require('express')
 const { resolve } = require('path')
 const app = express()
 
-app.use('/',
-  express.static(
-    resolve(
-      __dirname,
-      './build'
-    )
-  )
-)
+    app.use(express.static(
+        resolve(
+            __dirname,
+            './build'
+        )
+    ))
+    app.get('*', (req, res) => {
+        res.sendFile(resolve(__dirname, 'build', 'index.html'));
+      });
 
-app
-  .listen(process.env.PORT || 3000, (err) => {
+app.listen(process.env.PORT || 3000, (err) => {
     if (err) { return console.log(err) }
-
-    console.log('Tudo funcionando certinho')
-  })
+    console.log("tudo funcionando")
+})
